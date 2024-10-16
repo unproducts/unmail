0ed78cc4488bc934cc85a263f11a7350f515f8a0
amandesai01 <amandesai01@gmail.com>
2024-10-16 15:08:58 +0530

    chore(playground): move playground to mailersend

--- playground/mailersend.ts
+++ playground/mailersend.ts
@@ -6,34 +6,34 @@ dotenv.config();
 
 const TEST_TARGET_EMAIL = process.env.TEST_TARGET_EMAIL!;
 
-const MAILER_SEND_KEY = process.env.MAILERSEND_TOKEN!;
-const MAILER_SEND_DOMAIN = process.env.MAILERSEND_DOMAIN!;
+const KEY = process.env.MAILERSEND_TOKEN!;
+const DOMAIN = process.env.MAILERSEND_DOMAIN!;
 
 (async () => {
-  const mailerSendDriver = new MailerSendDriver({
-    auth: MAILER_SEND_KEY,
+  const driver = new MailerSendDriver({
+    auth: KEY,
   });
 
   const mailer = await createUnmail({
-    driver: mailerSendDriver,
+    driver,
   });
 
   const r1 = mailer.sendMail({
-    from: { email: `test-unmail@${MAILER_SEND_DOMAIN}` },
+    from: { email: `test-unmail@${DOMAIN}` },
     subject: 'This is test email',
     to: [{ email: TEST_TARGET_EMAIL }],
     text: 'This is test email',
   });
 
   const r2 = mailer.sendMail({
-    from: { email: `test-unmail@${MAILER_SEND_DOMAIN}` },
+    from: { email: `test-unmail@${DOMAIN}` },
     subject: 'This is test email',
     to: [{ email: TEST_TARGET_EMAIL }],
     html: '<b>This is test email</b>',
   });
 
   const r3 = mailer.sendMail({
-    from: { email: `test-unmail@${MAILER_SEND_DOMAIN}` },
+    from: { email: `test-unmail@${DOMAIN}` },
     subject: 'This is test email',
     to: [{ email: TEST_TARGET_EMAIL }],
     templateId: '3yxj6ljoxp54do2r',
