27d8226df396b79d03c7bda7219db483aa372242
amandesai01 <amandesai01@gmail.com>
2024-10-17 14:39:18 +0530

    chore(build): fix build config and distribution

---
+++ build.config.ts
@@ -0,0 +1,23 @@
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+  },
+  entries: [
+    'src/index',
+    {
+      input: 'src/drivers/',
+      outDir: 'drivers',
+      format: 'esm',
+    },
+    {
+      input: 'src/drivers/',
+      outDir: 'drivers',
+      format: 'cjs',
+      ext: 'cjs',
+      declaration: false,
+    },
+  ],
+});
